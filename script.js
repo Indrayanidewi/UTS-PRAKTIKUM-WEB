b5b81369d7905a41a1fdad12cc3cf020afaa631d
indrayanidewi <indrayanidewi6@gmail.com>
2022-04-12 17:54:39 +0800

    box mengikuti cursor

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.onmousemove = (event) => {
     box.style.left = event.clientX - (box.clientWidth/2) + 'px'; 
     box.style.top  = event.clientY - (box.clientHeight/2)+ 'px';
 }
-function random(number) {
-    return Math.floor(Math.random()* number)
-}
-document.onclick  = function () { 
-acak = "rgb("+ random(255)+","+ random(255)+","+ random(255)+")";
-box.style.background = acak
-}
+// function random(number) {
+//     return Math.floor(Math.random()* number)
+// }
+// document.onclick  = function () { 
+// acak = "rgb("+ random(255)+","+ random(255)+","+ random(255)+")";
+// box.style.background = acak
+// }
